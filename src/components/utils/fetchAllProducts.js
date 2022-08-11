729e41f26609af52f63280f0d2b3a2811bec28b1
Mike <mike.dorival@benextcompany.com>
2022-08-11 16:37:40 +0200

    install react-router and update files

--- src/components/utils/fetchAllProducts.js
+++ src/components/utils/fetchAllProducts.js
@@ -1,6 +1,12 @@
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
-export async function fetchAllProducts() {
+async function fetchAllProducts() {
   const { data } = await axios.get("https://fakestoreapi.com/products");
   return data;
+}
+
+export function useAllProducts() {
+  const { data, isLoading, isError } = useQuery(["allProducts"], fetchAllProducts);
+  return { data, isLoading, isError };
 }
